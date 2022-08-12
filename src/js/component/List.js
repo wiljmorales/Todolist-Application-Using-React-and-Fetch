790d11af6b4f96c00c5d744aaa93b315a3d4a85d
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-08-12 20:11:19 +0000

    :fire: Challenge Complete

--- src/js/component/List.js
+++ src/js/component/List.js
@@ -1,10 +1,80 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Todo } from "./Todo";
 import { TodoCount } from "./TodoCount";
 
 export const List = () => {
-	const [todoslist, setTodoslist] = useState(["Do my bed"]);
+	const [todoslist, setTodoslist] = useState([]);
 	const [inputValue, setImputValue] = useState("");
+
+	const createUser = useCallback(async () => {
+		try {
+			const response = await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/wiljmorales",
+				{
+					method: "post",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify([]),
+				}
+			);
+			if (response.status === 200) {
+				getToDos();
+			}
+		} catch (error) {
+			alert("no se pudo crear usuario", error);
+		}
+	}, [getToDos]);
+
+	const getToDos = async () => {
+		try {
+			const response = await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/wiljmorales"
+			);
+			if (response.status === 404) {
+				await createUser();
+				return;
+			}
+			const body = await response.json();
+			setTodoslist(body);
+		} catch (error) {
+			alert("la lista no esta");
+			return;
+		}
+	};
+
+	const updateTodos = async (requestBody) => {
+		try {
+			const response = await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/wiljmorales",
+				{
+					method: "PUT",
+					body: JSON.stringify(requestBody),
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+		} catch (error) {
+			alert("No se pudo actualizar la lista", error);
+			return;
+		}
+	};
+
+	const deleteTodoList = async () => {
+		try {
+			const response = await fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/wiljmorales",
+				{
+					method: "DELETE",
+				}
+			);
+		} catch (error) {
+			alert("no se pudo borrar la lista", error);
+		}
+	};
+
+	useEffect(() => {
+		getToDos();
+	}, []);
 
 	const onChangeHandler = (e) => {
 		setImputValue(e.target.value);
@@ -13,17 +83,24 @@ export const List = () => {
 	const addNewTask = (e) => {
 		if (e.key === "Enter") {
 			const newTodosList = [...todoslist];
-			newTodosList.push(inputValue);
+			newTodosList.push({ label: inputValue, done: false });
+			console.log(newTodosList);
 			setTodoslist(newTodosList);
+			updateTodos(newTodosList);
 			setImputValue("");
 		}
 	};
 
 	const deleteTodo = (text) => {
 		const newTodosList = [...todoslist];
-		const todoIndex = newTodosList.findIndex((todo) => todo === text);
+		const todoIndex = newTodosList.findIndex((todo) => todo.label === text);
 		newTodosList.splice(todoIndex, 1);
 		setTodoslist(newTodosList);
+		if (todoslist.length > 1) {
+			updateTodos(newTodosList);
+		} else {
+			deleteTodoList();
+		}
 	};
 
 	return (
--- src/js/component/Todo.js
+++ src/js/component/Todo.js
@@ -3,10 +3,10 @@ import React from "react";
 export const Todo = ({ todo, deleteTodo }) => {
 	return (
 		<li className="list-group-item d-flex justify-content-between ">
-			<p className="m-0">{todo}</p>
+			<p className="m-0">{todo.label}</p>
 			<i
 				className="fa-solid fa-trash-can mt-1"
-				onClick={() => deleteTodo(todo)}></i>
+				onClick={() => deleteTodo(todo.label)}></i>
 		</li>
 	);
 };
